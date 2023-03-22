01d09c6ae23527f04e7f084c13010aefff4c9116
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-22 18:54:10 -0300

    Req 02 OK

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('test', 'a')
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
+
+    assert encrypt_message('encrypt', 3) == 'cne_tpyr'
+    assert encrypt_message('message', 4) == 'ega_ssem'
+    assert encrypt_message('arara', 5) == 'arara'
