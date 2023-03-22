c8676477d7441ef857b175e2077dff172fd118ce
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-22 15:26:20 -0300

    Req 01 OK

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui"""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    count = 0
+
+    for student_schedule in permanence_period:
+        if not isinstance(
+            student_schedule[0], int
+        ) or not isinstance(student_schedule[1], int):
+            return None
+
+        if student_schedule[0] <= target_time <= student_schedule[1]:
+            count += 1
+
+    return count
